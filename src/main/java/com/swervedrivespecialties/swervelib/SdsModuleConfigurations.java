c7bfdae10e5ba871658ea648340189cff8be05ec
Royce <94499150+Dinodigger3000@users.noreply.github.com>
2022-11-15 17:05:55 -0600

    bad commit, please undo, for testing only

--- src/main/java/com/swervedrivespecialties/swervelib/SdsModuleConfigurations.java
+++ src/main/java/com/swervedrivespecialties/swervelib/SdsModuleConfigurations.java
@@ -50,7 +50,8 @@ public final class SdsModuleConfigurations {
             (14.0 / 50.0) * (25.0 / 19.0) * (15.0 / 45.0),
             true,
             (14.0 / 50.0) * (10.0 / 60.0),
-            false
+            false,
+            "Swerve"
     );
     public static final ModuleConfiguration MK4I_L2 = new ModuleConfiguration(
             0.10033,
